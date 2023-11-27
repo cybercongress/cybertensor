089c76637ef8f91a86658d052c86394b03670dbe
C H <litvintech@gmail.com>
2023-11-27 19:04:21 +0800

    Migration to cosmos wallet

---
+++ cybertensor/__init__.py
@@ -0,0 +1,142 @@
+# The MIT License (MIT)
+# Copyright © 2021 Yuma Rao
+# Copyright © 2022-2023 Opentensor Foundation
+# Copyright © 2023 Opentensor Technologies Inc
+# Copyright © 2023 cyber~Congress
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from rich.console import Console
+from rich.traceback import install
+
+# Install and apply nest asyncio to allow the async functions
+# to run in a .ipynb
+import nest_asyncio
+
+nest_asyncio.apply()
+
+# Cybertensor code and protocol version.
+__version__ = "0.1.0"
+version_split = __version__.split(".")
+__version_as_int__ = (
+    (100 * int(version_split[0]))
+    + (10 * int(version_split[1]))
+    + (1 * int(version_split[2]))
+)
+__new_signature_version__ = 360
+
+# Rich console.
+__console__ = Console()
+__use_console__ = True
+
+# Remove overdue locals in debug training.
+install(show_locals=False)
+
+
+def turn_console_off():
+    global __use_console__
+    global __console__
+    from io import StringIO
+
+    __use_console__ = False
+    __console__ = Console(file=StringIO(), stderr=False)
+
+
+def turn_console_on():
+    global __use_console__
+    global __console__
+    __use_console__ = True
+    __console__ = Console()
+
+
+turn_console_off()
+
+
+# Logging helpers.
+def trace(on: bool = True):
+    logging.set_trace(on)
+
+
+def debug(on: bool = True):
+    logging.set_debug(on)
+
+
+# Substrate chain block time (seconds).
+__blocktime__ = 6
+
+# Pip address for versioning
+__pipaddress__ = "https://pypi.org/pypi/cybertensor/json"
+
+# Raw github url for delegates registry file
+# __delegates_details_url__: str = "https://raw.githubusercontent.com/opentensor/bittensor-delegates/main/public/delegates.json"
+
+# Bostrom network address prefix
+__chain_address_prefix__ = "bostrom"
+
+__networks__ = ["local", "bostrom", "space-pussy"]
+
+# __local_entrypoint__ = "ws://127.0.0.1:9944"
+
+# __finney_entrypoint__ = "wss://entrypoint-finney.opentensor.ai:443"
+#
+# __finney_test_entrypoint__ = "wss://test.finney.opentensor.ai:443/"
+
+# change to emoji here
+__boot_symbol__: str = chr(0x03C4)
+
+__oxygen_symbol__: str = chr(0x03C4)
+
+# Block Explorers map network to explorer url
+# TODO update explorer presets
+__network_explorer_map__ = {
+    "local": "https://cyb.ai",
+    "bostrom": "https://cyb.ai",
+    "space-pussy": "https://cyb.ai",
+}
+
+from .errors import *
+
+from .config import *
+from .keyfile import *
+from .keypair import *
+from .wallet import *
+
+from .utils import *
+# from .utils.balance import Balance as Balance
+# from .chain_data import *
+from .cwtensor import cwtensor as cwtensor
+from .cli import cli as cli, COMMANDS as ALL_COMMANDS
+from .ctlogging import logging as logging
+# from .metagraph import metagraph as metagraph
+# from .threadpool import PriorityThreadPoolExecutor as PriorityThreadPoolExecutor
+
+# from .synapse import *
+# from .stream import *
+# from .tensor import *
+# from .axon import axon as axon
+# from .dendrite import dendrite as dendrite
+
+# from .mock.keyfile_mock import MockKeyfile as MockKeyfile
+# from .mock.subtensor_mock import MockSubtensor as MockSubtensor
+# from .mock.wallet_mock import MockWallet as MockWallet
+
+configs = [
+    # axon.config(),
+    cybertensor.config(),
+    # PriorityThreadPoolExecutor.config(),
+    wallet.config(),
+    logging.config(),
+]
+defaults = config.merge_all(configs)
---
+++ cybertensor/utils/__init__.py
@@ -0,0 +1,51 @@
+# The MIT License (MIT)
+# Copyright © 2022 Opentensor Foundation
+# Copyright © 2023 Opentensor Technologies Inc
+# Copyright © 2023 cyber~Congress
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from .wallet_utils import *
+import cybertensor
+import requests
+
+def version_checking(timeout: int = 15):
+    try:
+        cybertensor.logging.debug(
+            f"Checking latest Bittensor version at: {cybertensor.__pipaddress__}"
+        )
+
+        # TODO update when will be released
+        # response = requests.get(cybertensor.__pipaddress__, timeout=timeout)
+        # latest_version = response.json()["info"]["version"]
+        # version_split = latest_version.split(".")
+        # latest_version_as_int = (
+        #     (100 * int(version_split[0]))
+        #     + (10 * int(version_split[1]))
+        #     + (1 * int(version_split[2]))
+        # )
+        #
+        # if latest_version_as_int > cybertensor.__version_as_int__:
+        #     print(
+        #         "\u001b[33mCybertensor Version: Current {}/Latest {}\nPlease update to the latest version at your earliest convenience. "
+        #         "Run the following command to upgrade:\n\n\u001b[0mpython -m pip install --upgrade cybertensor".format(
+        #             cybertensor.__version__, latest_version
+        #         )
+        #     )
+
+    except requests.exceptions.Timeout:
+        cybertensor.logging.error("Version check failed due to timeout")
+    except requests.exceptions.RequestException as e:
+        cybertensor.logging.error(f"Version check failed due to request failure: {e}")
