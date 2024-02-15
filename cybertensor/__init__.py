f43dcd67fc88be9971e60e3dea09dd1cfed24a9e
Snedashkovsky <sn@cyberdrop.ai>
2024-02-15 13:38:57 +0800

    - bump the package version

--- cybertensor/__init__.py
+++ cybertensor/__init__.py
@@ -30,14 +30,13 @@ from rich.traceback import install
 nest_asyncio.apply()
 
 # Cybertensor code and protocol version.
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 version_split = __version__.split(".")
 __version_as_int__ = (
     (100 * int(version_split[0]))
     + (10 * int(version_split[1]))
     + (1 * int(version_split[2]))
 )
-__new_signature_version__ = 360
 
 # Rich console.
 __console__ = Console()
