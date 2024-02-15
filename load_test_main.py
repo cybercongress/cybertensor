fac0f6e414787d1a852e98d5ba680443ff827299
Snedashkovsky <sn@cyberdrop.ai>
2024-02-15 15:51:41 +0800

    - add arguments in load test

--- load_test_main.py
+++ load_test_main.py
@@ -15,6 +15,11 @@
 # DEALINGS IN THE SOFTWARE.
 
 
+# Example usage:
+# >>> python load_test_main.py --wallets=15 --skip-wallets=10 --send-to-contract=y --send-to-wallets=y \
+# --register-subnet=n --root-register=y --subnet-register=y --subnet-nominate=n --self-stake=y --self-unstake=n \
+# --set-root-weight=y --set-subnets-weight=y```
+
 import warnings
 from argparse import ArgumentParser
 from random import sample, randint
@@ -44,18 +49,64 @@ class Account(TypedDict):
 BASE_WALLET_SEED = "notice oak worry limit wrap speak medal online prefer cluster roof addict wrist behave treat actual wasp year salad speed social layer crew genius"
 
 parser = ArgumentParser()
-parser.add_argument("--addresses", default=10)
-parser.add_argument("--send-to-contract", default="false")
-parser.add_argument("--activate-contract", default="false")
-parser.add_argument("--send-to-wallets", default="true")
-parser.add_argument("--threads", default=5)
+parser.add_argument("--wallets", default=10, type=int, help="Number of wallets")
+parser.add_argument("--skip-wallets", default=0, type=int, help="Skip wallets")
+parser.add_argument(
+    "--send-to-contract", default="false", type=str, help="Send tokens to the contract"
+)
+parser.add_argument(
+    "--activate-contract",
+    default="false",
+    type=str,
+    help="Activate dmn thought for the contract",
+)
+parser.add_argument(
+    "--send-to-wallets", default="true", type=str, help="Send tokens to wallets"
+)
+parser.add_argument("--threads", default=5, type=int, help="Number of threads")
+parser.add_argument(
+    "--register-subnet", default="false", type=str, help="Register new subnetworks"
+)
+parser.add_argument(
+    "--root-register", default="true", type=str, help="Register wallets in root"
+)
+parser.add_argument(
+    "--subnet-register",
+    default="true",
+    type=str,
+    help="Register wallets in subnetworks",
+)
+parser.add_argument(
+    "--subnet-nominate",
+    default="false",
+    type=str,
+    help="Nominate wallets in subnetworks",
+)
+parser.add_argument("--self-stake", default="true", type=str, help="Self stake")
+parser.add_argument("--self-unstake", default="false", type=str, help="Self unstake")
+parser.add_argument(
+    "--set-root-weight", default="true", type=str, help="Set roots' weights"
+)
+parser.add_argument(
+    "--set-subnets-weight", default="false", type=str, help="Set subnets' weights"
+)
 args = parser.parse_args()
 
-ADDRESSES_NUMBER = int(args.addresses)
+
+NUMBER_OF_WALLETS = int(args.wallets)
+SKIP_WALLETS = int(args.skip_wallets)
 SEND_TOKEN_TO_CONTRACT = args.send_to_contract.lower() in ["true", "1", "t", "y", "yes"]
 ACTIVATE_CONTRACT = args.activate_contract.lower() in ["true", "1", "t", "y", "yes"]
 SEND_TOKEN_TO_WALLETS = args.send_to_wallets.lower() in ["true", "1", "t", "y", "yes"]
 NUMBER_OF_THREADS = int(args.threads)
+REGISTER_SUBNET = args.register_subnet.lower() in ["true", "1", "t", "y", "yes"]
+REGISTER_IN_ROOT = args.root_register.lower() in ["true", "1", "t", "y", "yes"]
+REGISTER_IN_SUBNET = args.subnet_register.lower() in ["true", "1", "t", "y", "yes"]
+NOMINATE_IN_SUBNET = args.subnet_nominate.lower() in ["true", "1", "t", "y", "yes"]
+SELF_STAKE = args.self_stake.lower() in ["true", "1", "t", "y", "yes"]
+SELF_UNSTAKE = args.self_unstake.lower() in ["true", "1", "t", "y", "yes"]
+SET_ROOT_WEIGHT = args.set_root_weight.lower() in ["true", "1", "t", "y", "yes"]
+SET_SUBNETS_WEIGHT = args.set_subnets_weight.lower() in ["true", "1", "t", "y", "yes"]
 
 
 def send_coins(
@@ -88,9 +139,11 @@ def activate_contract() -> None:
         print(_tx)
 
 
-def get_accounts(addresses_number: int = ADDRESSES_NUMBER) -> list[Account]:
+def get_accounts(
+    number_of_wallets: int = NUMBER_OF_WALLETS, skip_wallets: int = SKIP_WALLETS
+) -> list[Account]:
     _accounts: list[Account] = []
-    for _i in range(1, addresses_number + 1):
+    for _i in range(skip_wallets, number_of_wallets):
         _wallet_name = f"wallet{_i}"
         _wallet = Wallet(name=_wallet_name, hotkey=_wallet_name, path="temp")
         _wallet.create_if_non_existent(
@@ -117,17 +170,17 @@ def get_accounts(addresses_number: int = ADDRESSES_NUMBER) -> list[Account]:
 def workflow(
     account: Account,
     netuids: Optional[list[int]] = None,
-    register_subnetwork: bool = True,
-    root_register: bool = True,
-    subnet_register: bool = True,
-    subnet_nominate: bool = True,
-    self_stake: bool = True,
+    register_subnetwork: bool = REGISTER_SUBNET,
+    root_register: bool = REGISTER_IN_ROOT,
+    subnet_register: bool = REGISTER_IN_SUBNET,
+    subnet_nominate: bool = NOMINATE_IN_SUBNET,
+    self_stake: bool = SELF_STAKE,
     self_stake_amount: float = 1,
-    self_unstake: bool = True,
+    self_unstake: bool = SELF_UNSTAKE,
     self_unstake_amount: float = 0.1,
-    root_set_weight: bool = True,
+    root_set_weight: bool = SET_ROOT_WEIGHT,
     root_weights: Optional[list[float]] = None,
-    subnets_set_weight: bool = True,
+    subnets_set_weight: bool = SET_SUBNETS_WEIGHT,
     subnets_weights: Optional[dict[int, list[list[int], list[float]]]] = None,
 ) -> None:
     sleep(randint(0, 40))
@@ -161,10 +214,7 @@ def workflow(
         if root_set_weight:
             if root_weights is None:
                 root_weights = sample(range(1, 1 + len(_subnetuids)), len(_subnetuids))
-                print(
-                    f"netuids: {_subnetuids}\t"
-                    f"root_weights: {root_weights}"
-                )
+                print(f"netuids: {_subnetuids}\troot_weights: {root_weights}")
             _tensor.root_set_weights(
                 wallet=account["wallet"],
                 netuids=_subnetuids,
@@ -237,10 +287,20 @@ if __name__ == "__main__":
     print(
         f"base address: {base_wallet_address}\n"
         f"base address balance: {tensor.get_balance(address=base_wallet_address)}\n"
-        f"number of wallets: {ADDRESSES_NUMBER}\n"
         f"send token to the contract: {SEND_TOKEN_TO_CONTRACT}\n"
         f"activate contract: {ACTIVATE_CONTRACT}\n"
-        f"send token to wallets: {SEND_TOKEN_TO_WALLETS}\n"
+        f"number of wallets: {NUMBER_OF_WALLETS}\n"
+        f"number of skipped wallets: {SKIP_WALLETS}\n"
+        f"send token to the wallets: {SEND_TOKEN_TO_WALLETS}\n"
+        f"number of threads: {NUMBER_OF_THREADS}\n"
+        f"register new subnets: {REGISTER_SUBNET}\n"
+        f"register in root: {REGISTER_IN_ROOT}\n"
+        f"register in subnets: {REGISTER_IN_SUBNET}\n"
+        f"nominate in subnets: {NOMINATE_IN_SUBNET}\n"
+        f"self stake: {SELF_STAKE}\n"
+        f"self unstake: {SELF_UNSTAKE}\n"
+        f"set root weights: {SET_ROOT_WEIGHT}\n"
+        f"set subnets weights: {SET_SUBNETS_WEIGHT}\n"
     )
 
     send_token_to_contract()
